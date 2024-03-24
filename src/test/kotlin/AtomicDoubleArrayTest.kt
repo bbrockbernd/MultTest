0c86e12a8e7d90a8c12f664bbe6eb317ddfc137a
Bob Brockbernd <bobbrockbernd@gmail.com>
2024-03-24 12:31:42 +0100

    added params to test

--- src/test/kotlin/AtomicDoubleArrayTest.kt
+++ src/test/kotlin/AtomicDoubleArrayTest.kt
@@ -1,24 +1,27 @@
 import com.google.common.util.concurrent.AtomicDoubleArray
 import org.jetbrains.kotlinx.lincheck.annotations.Operation
+import org.jetbrains.kotlinx.lincheck.annotations.Param
 import org.jetbrains.kotlinx.lincheck.check
+import org.jetbrains.kotlinx.lincheck.paramgen.IntGen
 import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingOptions
 import org.junit.Test
 
+@Param(name = "index", gen = IntGen::class, conf = "1:2")
 class AtomicDoubleArrayTest {
-    private val array: AtomicDoubleArray = AtomicDoubleArray(10)
+    private val array: AtomicDoubleArray = AtomicDoubleArray(3)
 
     @Operation
-    fun set(index: Int, value: Double) {
+    fun set(@Param(name = "index") index: Int, value: Double) {
         array[index] = value
     }
 
     @Operation
-    fun get(index: Int): Double {
+    fun get(@Param(name = "index") index: Int): Double {
         return array[index]
     }
 
     @Operation
-    fun addAndGet(index: Int, delta: Double): Double {
+    fun addAndGet(@Param(name = "index") index: Int, delta: Double): Double {
         return array.addAndGet(index, delta)
     }
 
